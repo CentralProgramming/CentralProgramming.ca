95b46418f1bb1f36db0c8cc1827424f9ae3dad38
mcdoash <mcdoash@users.noreply.github.com>
2017-09-09 23:07:51 -0400

    SEO and other optimizations

--- templates/header.php
+++ templates/header.php
@@ -5,15 +5,56 @@
         <!--META DATA-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
-        <meta name="keywords" content="central programming club, programming club, central secondary, central clubs, london central">
-        <meta name="description" content="London Central Secondary School's Programming Club.">
+        <meta name="keywords" content="central programming club, london central, lcss, london ontario">
+        <meta name="description" content="<?php
+                                     if (basename($_SERVER['PHP_SELF']) == 'index.php'){
+                                        echo 'London Central Secondary School&apos;s Programming Club.';
+                                     }
+                                     else if(basename($_SERVER['PHP_SELF']) == 'problems.php'){
+                                        echo 'Practice your coding skills with Central Programming Club&apos;s weekly problems.';
+                                     }
+                                     else if(basename($_SERVER['PHP_SELF']) == 'contests.php'){
+                                        echo 'Information about all of the computer contests Central Programming Club will be participating in this school year.';
+                                     }
+                                     else if(basename($_SERVER['PHP_SELF']) == 'resources.php'){
+                                        echo 'Resources for learning Java and other programming languages by Central Programming Club.';
+                                     }
+                                     else if(basename($_SERVER['PHP_SELF']) == 'calendar.php'){
+                                        echo 'A calendar of events for Central Programming Club in 2017/2018.';
+                                     }
+                                     else if(basename($_SERVER['PHP_SELF']) == 'contact.php'){
+                                        echo 'A contact form to easily reach the Central Programming Club team.';
+                                     }
+                                     ?>">
         <meta name="author" content="Central Programming">
         
         <!--FONTS-->
         <link href="https://fonts.googleapis.com/css?family=Monoton%7CPatua+One%7CGraduate%7CNunito:300,400,700%7C" rel="stylesheet">
         
         <!--TAB TITLE AND ICON-->
-        <title>Central Programming</title>
+        <title>
+            <?php
+                if(basename($_SERVER['PHP_SELF']) == 'problems.php'){
+                    echo 'Weekly Porblems';
+                 }
+                 else if(basename($_SERVER['PHP_SELF']) == 'contests.php'){
+                    echo 'Computer Contests';
+                 }
+                 else if(basename($_SERVER['PHP_SELF']) == 'resources.php'){
+                    echo 'Programming Resources';
+                 }
+                 else if(basename($_SERVER['PHP_SELF']) == 'calendar.php'){
+                    echo 'Calander';
+                 }
+                 else if(basename($_SERVER['PHP_SELF']) == 'contact.php'){
+                    echo 'Contact Form';
+                 }
+                if(basename($_SERVER['PHP_SELF']) != 'index.php'){
+                    echo ' &verbar; ';
+                 }
+                 echo 'Central Programming Club';
+                 ?>
+        </title>
         <link rel="shortcut icon" href="images/central-logo.png">
         
         <!--STYLESHEET AND JAVASCRIPT LINKS-->
@@ -68,7 +109,7 @@
             <header>
                 <div class="parallax" id="header-image">
                 <hr style="height: 150px; border: 0;">
-                    <img id="logo" src="images/logo.png">
+                    <img id="logo" src="images/central-programming-club-logo.png" alt="Central Programming Club's Logo">
                     <div id="title">
                         <h1 id="central"><span class="purple">C</span>entral</h1>
                         <h1 id="programming"><span class="purple">P</span>rogramming</h1>
