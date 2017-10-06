c130f8e2cf45cc87f00b6d9b20c0a0625c6fd12a
mcdoash <mcdoash@users.noreply.github.com>
2017-10-05 20:45:37 -0400

    SQL leaderboard (finally)

--- problems.php
+++ problems.php
@@ -2,7 +2,10 @@
 <?php require_once "templates/header.php"; ?>
             
 <!--NAV & ALERT-->
-<?php require_once "templates/nav.php"; ?>
+<?php require_once "templates/nav.php"; ?> 
+
+<!--LEADERBOARD SCRIPT-->
+<?php require_once "scripts/problemscript.php"; ?>
 
             
             <div id="top">
@@ -12,8 +15,8 @@
                     <section id="about">
                         <div id="about-content">
                             <h2>Purpose</h2><br/>
-                            <p>Programming problems are a great way to prepare for contests, practice your coding skills, and exercise problem solving. With that in mind, a new problem from <a href="https://dmoj.ca/">DMOJ</a> will be posted here every Sunday(?). By participating in and solving these problems, you will earn points that go towards monthly and year-end prizes. You don't have to make a commitment to participate every week <span class="light">(because who had time for that at Central?)</span>; simply try a problem at home when you have time, or come in during one of our meetings and solve it with the help of your peers.</p><br/>
-                            <p><span class="strong">Last week's top scorer: </span><b>N/A</b></p>
+                            <p>Programming problems are a great way to prepare for contests, practice your coding skills, and exercise problem solving. With that in mind, a new problem from <a href="https://dmoj.ca/" target="_blank">DMOJ</a> will be posted here every Thursday. By participating in and solving these problems, you will earn points that go towards monthly and year-end prizes. You don't have to make a commitment to participate every week <span class="light">(because who had time for that at Central?)</span>; simply try a problem at home when you have time, or come in during one of our meetings and solve it with the help of your peers.</p><br/>
+                            <!--<p><span class="strong">Last week's top scorer: </span><b><a href="https://dmoj.ca/user/_Zaman" target="_blank"></a></b></p>-->
                         </div>
                     </section>
 
@@ -22,14 +25,15 @@
                     <section id="this-week">
                         <h2>This Week's Problem</h2>
                         <div class="prob">
-                            <h3><a href="https://dmoj.ca/problem/ccc02j1" target="_blank">CCC '02 J1 - 0123456789
+                            <h3><a href="https://dmoj.ca/problem/ccc12j2" target="_blank">CCC '12 J2 - Sounds fishy!
+
  &raquo;</a></h3>
                             <ul>
-                                <li><b>Points:</b> 5</li>
+                                <li><b>Points:</b> 3</li>
                                 <li><b>Time limit:</b> 2.0s</li>
                                 <li><b>Author:</b> CCC</li>
                             </ul>
-                            <br/><p>From input of a number from 0-9, output the digit using asterisks.</p>
+                            <br/><p>From input of four "depth readings", determine whether fish are rising, diving, or swimming at a constant depth.</p>
                         </div>
                     </section>
                 </div>
@@ -40,154 +44,66 @@
                 <section id="leaderboard">
                     <div id="leader-title">
                         <h2>Leaderboard</h2>
-                        <h3>Last Updated: 23/9/17</h3>
                     </div>
                     <table>
                         <tr>
                             <th>Rank</th>
                             <th>Username</th>
-                            <th>Points</th>
+                            <th><a href="#disclaimer">Points <sup>*</sup></a></th>
                         </tr>
-                        <tr>
-                            <td class="rank">01</td>
-                            <td class="user"><a href="https://dmoj.ca/user/_Zaman" target="_blank">_Zaman</a></td>
-                            <td class="points">5</td>
-                        </tr>
-                        <tr>
-                            <td class="rank">02</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">03</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">04</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">05</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">06</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">07</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">08</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">09</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
-                        <tr>
-                            <td class="rank">10</td>
-                            <td class="user"></td>
-                            <td class="points"></td>
-                        </tr>
+                        <?php
+                            $rank = 1;
+                                
+                            for($x=0; $x<=9; $x++){
+                                echo '<tr>
+                                        <td class="rank">' . sprintf('%02d', $rank) . '</td>
+                                        <td class="user"><a href="https://dmoj.ca/user/' . $user[$x] . '" target="_blank">' . $user[$x] . '</a></td>
+                                        <td class="points">' . $points[$x] . '</td>
+                                    </tr>';
+                                
+                                 if($points[$x+1]!=$points[$x] || $points[$x+1]==NULL){
+                                     $rank++;   
+                                 }
+                            }
+                        ?>
+            
                     </table>
+                
+                    <h3 id="disclaimer"><sup>*</sup> Points are subject to change. We're still working on how we score problems.</h3>
                 </section>
             </div>
             
             
             
-            <!--PAST PROBLEMS
+            <!--PAST PROBLEMS-->
             <hr style="height: 30px; border: 0;">
             
             <h2>Past Problems</h2>
-            <hr style="height: 20px; border: 0;">
+            <hr style="height: 25px; border: 0;">
+
             <section id="past-probs">
-    
-                <div class="column">
-                    <div class="prob">
-                        <p class="date"><b>Date: </b>1/1/17</p><br/>
-                        <h3><a href="https://dmoj.ca/problems/">Problem Name &raquo;</a></h3>
-                        <ul>
-                            <li><b>Points:</b> #</li>
-                            <li><b>Time limit:</b> #s</li>
-                            <li><b>Author:</b> name</li>
-                        </ul>
-                        <br/><p class="container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor magna quis risus iaculis, sed mollis mi consectetur.</p><br/>
-                        <p class="top-score"><b>Top Score: </b>user</p>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="prob">
-                        <p class="date"><b>Date: </b>1/1/17</p><br/>
-                        <h3><a href="https://dmoj.ca/problems/">Problem Name &raquo;</a></h3>
-                        <ul>
-                            <li><b>Points:</b> #</li>
-                            <li><b>Time limit:</b> #s</li>
-                            <li><b>Author:</b> name</li>
-                        </ul>
-                        <br/><p class="container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor magna quis risus iaculis, sed mollis mi consectetur.</p><br/>
-                        <p class="top-score"><b>Top Score: </b>user</p>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="prob">
-                        <p class="date"><b>Date: </b>1/1/17</p><br/>
-                        <h3><a href="https://dmoj.ca/problems/">Problem Name &raquo;</a></h3>
-                        <ul>
-                            <li><b>Points:</b> #</li>
-                            <li><b>Time limit:</b> #s</li>
-                            <li><b>Author:</b> name</li>
-                        </ul>
-                        <br/><p class="container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor magna quis risus iaculis, sed mollis mi consectetur.</p><br/>
-                        <p class="top-score"><b>Top Score: </b>user</p>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="prob">
-                        <p class="date"><b>Date: </b>1/1/17</p><br/>
-                        <h3><a href="https://dmoj.ca/problems/">Problem Name &raquo;</a></h3>
-                        <ul>
-                            <li><b>Points:</b> #</li>
-                            <li><b>Time limit:</b> #s</li>
-                            <li><b>Author:</b> name</li>
-                        </ul>
-                        <br/><p class="container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor magna quis risus iaculis, sed mollis mi consectetur.</p><br/>
-                        <p class="top-score"><b>Top Score: </b>user</p>
-                    </div>
-                </div>
-
-                <div class="column">
-                    <div class="prob">
-                        <p class="date"><b>Date: </b>1/1/17</p><br/>
-                        <h3><a href="https://dmoj.ca/problems/">Problem Name &raquo;</a></h3>
-                        <ul>
-                            <li><b>Points:</b> #</li>
-                            <li><b>Time limit:</b> #s</li>
-                            <li><b>Author:</b> name</li>
-                        </ul>
-                        <br/><p class="container">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tempor magna quis risus iaculis, sed mollis mi consectetur.</p><br/>
-                        <p class="top-score"><b>Top Score: </b>user</p>
-                    </div>
+                <div class="row">
+                    <?php
+                        for($x=0; $x<count($name); $x++){
+                            echo '<div class="prob">
+                            <p class="date"><b>Date: </b>' . $date[$x] . '</p><br/>
+                            <h3><a href="https://dmoj.ca/problem/' . $url[$x] . '" target="_blank">' . $name[$x] . ' &raquo;</a></h3>
+                            <ul>
+                                <li><b>Points:</b> ' . $points[$x] . '</li>
+                                <li><b>Time limit:</b> ' . number_format($limit[$x], 1) . '</li>
+                                <li><b>Author:</b> ' . $author[$x] . '</li>
+                            </ul>
+                            <br/><p>' . $description[$x] . '</p>' . /*'<br/>
+                            <p class="top-score"><b>Top Score: </b><a href="https://dmoj.ca/user/' . $top[$x] . '" target="_blank">' . $top[$x] . '</a></p>*/
+                        '</div>';
+        }
+                    ?>
                 </div>
             </section>
 
-            <hr style="height: 70px; border: 0;">
-            -->
+            <hr style="height: 100px; border: 0;">
             
-            <hr style="height: 40px; border: 0;">
             
-    
             
 <!--FOOTER & TOP BUTTON-->
 <?php require_once "templates/footer.php"; ?>    
---
+++ scripts/problemscript.php
@@ -0,0 +1,38 @@
+<?php
+require_once "../../private/sql-bits.php";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+    echo("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM leaderboard ORDER BY score DESC";
+$result = $conn->query($sql);
+
+if($result->num_rows >0){
+    while($row = $result->fetch_assoc()){
+        $user[] = $row["user"];
+        $points[] = $row["score"];
+    }
+}
+
+$sql = "SELECT * FROM past_probs ORDER BY ID DESC";
+$result = $conn->query($sql);
+
+if($result->num_rows >0){
+    while($row = $result->fetch_assoc()){
+        $date[] = $row["Prob_Date"];
+        $name[] = $row["Name"];
+        $url[] = $row["URL"];
+        $points[] = $row["Points"];
+        $limit[] = $row["Time_Limit"];
+        $author[] = $row["Author"];
+        $description[] = $row["Description"];
+        $top[] = $row["Top_Score"];
+    }
+}
+
+$conn->close();
+?>
